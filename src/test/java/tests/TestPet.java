0cc58446c964bd4a320eb0400abccca9aa52b811
aborisenok1 <alex.borisenok@gmail.com>
2026-06-28 14:51:19 +0300

    Добавил автотест на проверку обновления несуществующего питомца

--- src/test/java/tests/TestPet.java
+++ src/test/java/tests/TestPet.java
@@ -1,10 +1,12 @@
+package tests;
+
 import io.qameta.allure.Feature;
 import io.qameta.allure.Owner;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-import io.restassured.response.ResponseBody;
+import models.Pet;
 import org.junit.jupiter.api.Test;
 
 import static io.qameta.allure.Allure.step;
@@ -21,7 +23,7 @@ public class TestPet {
     @Severity(SeverityLevel.CRITICAL)
     @Owner("alex_borisenok")
     public void testNotExistedPet() {
-        Response response = step("Отправить DELETE запрос на удаление несуществующего Pet", () ->
+        Response response = step("Отправить DELETE запрос на удаление несуществующего питомца", () ->
                 given()
                         .contentType(ContentType.JSON)
                         .header("Accept", "application/json")
@@ -38,12 +40,44 @@ public class TestPet {
 /*        assertEquals(200, response.getStatusCode(),
                 "Код ответа не совпал с ожидаемым. Ответ " + responseBody);*/
 
-        step("Проверить, что текст ответа 'Pet deleted'", () ->
+        step("Проверить, что текст ответа 'models.Pet deleted'", () ->
                 assertEquals("Pet deleted", responseBody,
                         "Текст ошибки не совпал с ожидаемым. Получен: " + responseBody)
         );
 
-/*        assertEquals("Pet deleted", responseBody,
+/*        assertEquals("models.Pet deleted", responseBody,
                 "Текст ошибки не совпал с ожидаемым. Получен: " + responseBody);*/
+    }
+
+    @Test
+    @Feature("Pet")
+    @Severity(SeverityLevel.CRITICAL)
+    @Owner("alex_borisenok")
+    public void testUpdateNoneExistentPet() {
+        Pet pet = new Pet();
+        pet.setId(9999);
+        pet.setName("Non-existent Pet");
+        pet.setStatus("available");
+
+        Response response = step("Отправить PUT запрос на обновление несуществующего питомца", () ->
+                given()
+                        .contentType(ContentType.JSON)
+                        .header("Accept", "application/json")
+                        .body(pet)
+                        .when()
+                        .put(BASE_URL + "/pet"));
+
+        String responseBody = response.getBody().asString();
+
+        step("Проверить, что статус-код ответа == 404", () ->
+                assertEquals(404, response.getStatusCode(),
+                        "Код ответа не совпал с ожидаемым. Ответ " + responseBody)
+        );
+
+        step("Проверить, что текст ответа 'Pet not found'", () ->
+                assertEquals("Pet not found", responseBody,
+                        "Текст ошибки не совпал с ожидаемым. Получен: " + responseBody)
+        );
+
     }
 }
